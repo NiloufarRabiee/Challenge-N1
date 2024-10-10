768e57abc858735287d95e168b52b678470b20ac
Niloufar Rabiee <rabiee.niloo@gmail.com>
2024-10-10 15:36:45 +0200

    Initial Commit

---
+++ appetizers1/appetizers1App.swift
@@ -0,0 +1,17 @@
+//
+//  appetizers1App.swift
+//  appetizers1
+//
+//  Created by Niloufar Rabiee on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct appetizers1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
